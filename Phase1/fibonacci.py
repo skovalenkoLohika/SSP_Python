8072a479dbff19824b67fd6205839e647c0c4b8a
sergey kovalenko <sergey.kovalenko@hpe.com>
2018-08-27 19:16:23 +0300

    phase1

--- Phase1/fibonacci.py
+++ Phase1/fibonacci.py
@@ -1,13 +1,23 @@
 def main():
-    print(fib(5))
+    try:
+        n = int(input("Enter number of terms:"))
+        print(fib(n))
+    except ValueError:
+        print('you have inputted wrong number')
+
 
 
 def fib(num):
     fibo = [0, 1]
-    for i in range(num):
-            fibo.append(fibo[-2] + fibo[-1])
-    return fibo
-
+    if type(num) == int:
+        if num >=2:
+            for i in range(num):
+                fibo.append(fibo[-2] + fibo[-1])
+            return fibo
+        else:
+            print('please input more than 1')
+    else:
+        print('You can input only digits')
 
 if __name__ == '__main__':
     main()
