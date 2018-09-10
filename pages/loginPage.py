d83c9eb8b4f664eb56ae106d7768e8b1e1fd6c6b
sergey kovalenko <sergey.kovalenko@hpe.com>
2018-09-10 17:18:02 +0300

    code is refactored

--- pages/loginPage.py
+++ pages/loginPage.py
@@ -9,9 +9,12 @@ class LoginPage(Driver):
     username = driver.find_element_by_id('login-form-username')
     password = driver.find_element_by_id('login-form-password')
     create_button = driver.find_element_by_id("create_link")
+    login_button = driver.find_element_by_id("login-form-submit")
 
     def login(self, username, password):
+        driver.get("http://jira.hillel.it:8080/login.jsp")
         self.username.send_keys(username)
         self.password.send_keys(password)
+
         return self.create_button
 
